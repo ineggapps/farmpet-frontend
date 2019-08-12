48f57f1d4a5030eb01d914a94f562db4e2644a77
ineggapps <inegg.apps@gmail.com>
2019-08-12 23:48:43 +0900

    로그인 직후 토큰이 전달되지 않는 버그 수정

--- src/Apollo/LocalState.js
+++ src/Apollo/LocalState.js
@@ -1,15 +1,18 @@
 export const TOKEN = "token";
 export const defaults = {
-  isLoggedIn: Boolean(localStorage.getItem(TOKEN)) || false
+  isLoggedIn: Boolean(localStorage.getItem(TOKEN) !== null) || false
 };
 
 export const resolvers = {
   Mutation: {
     logUserIn: (_, { token }, { cache }) => {
+      //시작 페이지 고침 없으려면 redux를 써야 할 듯. bug.
+      window.location = "/";
       localStorage.setItem(TOKEN, token);
       cache.writeData({
         data: {
-          isLoggedIn: true
+          isLoggedIn: true,
+          getToken: token
         }
       });
       return null;
