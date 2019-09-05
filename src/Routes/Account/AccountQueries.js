cfea08bcc5541a1eb0bcca4413ae152953379949
ineggapps <inegg.apps@gmail.com>
2019-09-05 17:41:43 +0900

    bio에서 $bio로 문법 오류 수정

--- src/Routes/Account/AccountQueries.js
+++ src/Routes/Account/AccountQueries.js
@@ -1,7 +1,19 @@
 import { gql } from "apollo-boost";
 
-const UPDATE_ACCOUNT = gql`
-    mutation updateAccount($avatar:String, $username:String, $firstName:String, $lastName:String, bio:String){
-        updateAccount(avatar:$avatar, username:$username, firstName:$firstName, lastName:$lastName, bio:$bio)
-    }
+export const UPDATE_ACCOUNT = gql`
+  mutation updateAccount(
+    $avatar: String
+    $username: String
+    $firstName: String
+    $lastName: String
+    $bio: String
+  ) {
+    updateAccount(
+      avatar: $avatar
+      username: $username
+      firstName: $firstName
+      lastName: $lastName
+      bio: $bio
+    )
+  }
 `;
