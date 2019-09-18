e21988ad764097e850e155214ca3ac463dd4a58d
ineggapps <inegg.apps@gmail.com>
2019-09-18 19:17:29 +0900

    포트 수정

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers, TOKEN } from "./LocalState";
 
 export default new ApolloClient({
-  uri: "http://farmpet-backend.inegg.com:5000",
+  uri: "http://farmpet-backend.inegg.com:500",
   clientState: {
     defaults,
     resolvers
