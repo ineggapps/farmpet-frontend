e525eaa736613d491d19de505753facd200c74ef
ineggapps <inegg.apps@gmail.com>
2019-10-03 00:35:41 +0900

    git 충돌내역 수정 후 오기 수정

--- src/OverlayContext.js
+++ src/OverlayContext.js
@@ -9,9 +9,6 @@ const OverlayContextProvider = ({ children }) => {
     <OverlayContext.Provider value={{ isShow, setIsShow, opacity, setOpacity }}>
       {children}
     </OverlayContext.Provider>
-  return (
-    <OverlayContext.Provider value={{ isShow, setIsShow }}>{children}</OverlayContext.Provider>
-  );
 };
 
 export const useOverlay = () => {
