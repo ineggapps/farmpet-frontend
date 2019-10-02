9b1c283dd1270ee563a062ea68216d3e629e6def
ineggapps <inegg.apps@gmail.com>
2019-10-02 23:32:28 +0900

    AddOwner 컴포넌트 디자인 구성 시작

--- src/Components/AddOwner/AddOwnerPresenter.js
+++ src/Components/AddOwner/AddOwnerPresenter.js
@@ -1,8 +1,31 @@
 import React from "react";
 import styled from "styled-components";
 
+const Container = styled.div`
+  z-index: 4000; /* overlay가 z-index:3000임 */
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Component = styled.div`
+  width: 600px;
+  height: 450px;
+  background-color: white;
+  border-radius: 3px;
+`;
+
 const AddOwnerPresenter = () => {
-  return <div>hello world!</div>;
+  return (
+    <Container>
+      <Component>hello world!</Component>
+    </Container>
+  );
 };
 
 export default AddOwnerPresenter;
