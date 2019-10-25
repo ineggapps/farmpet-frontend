aee754cf58c1764197267b4ed5d8bbd31e89b1b9
ineggapps <inegg.apps@gmail.com>
2019-10-25 22:02:11 +0900

    Post 컴포넌트 작성 (해당 글만 조회할 수 있음, 컴포넌트 재활용)

--- src/Routes/Post.js
+++ src/Routes/Post.js
@@ -1,1 +1,132 @@
-export default () => "Post";
+import React, { useEffect } from "react";
+import Helmet from "react-helmet";
+import styled from "styled-components";
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo-hooks";
+import Post from "../Components/Post";
+import WritingToolBox from "../Components/WritingToolBox";
+import { ME } from "../SharedQueries";
+import SideProfile from "../Components/SideProfile";
+import SideUsers from "../Components/SideUsers";
+import SidePets from "../Components/SidePets";
+import { usePostGallery } from "../PostGalleryContext";
+import { withRouter } from "react-router-dom";
+import MainLoader from "../Components/MainLoader";
+
+const SEE_POST = gql`
+  query seePost($postId: String!) {
+    seePost(postId: $postId) {
+      id
+      user {
+        id
+        avatar
+        username
+      }
+      files {
+        id
+        url
+        thumbnail
+        caption
+      }
+      caption
+      pets {
+        id
+        category
+        name
+        avatar
+      }
+      comments {
+        id
+        text
+        user {
+          id
+          avatar
+          username
+        }
+        createdAt
+        updatedAt
+      }
+      createdAt
+      isLiked
+      likeCount
+      permission
+      likes {
+        id
+        user {
+          id
+          username
+          avatar
+        }
+      }
+      commentCount
+    }
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Contents = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+const SectionLeft = styled.section`
+  margin-right: 20px;
+`;
+const SNB = styled.section``;
+
+export default withRouter(({ match: { params: { postId } }, history }) => {
+  const { data, loading } = useQuery(SEE_POST, {
+    variables: { postId },
+    fetchPolicy: "cache-and-network"
+  });
+  const { data: meData, loading: meLoading } = useQuery(ME, { fetchPolicy: "cache-and-network" });
+
+  const LoaderContents = <MainLoader />;
+
+  const RealContents = (
+    <>
+      <SectionLeft>
+        {!meLoading && meData && meData.me && data && data.seePost && (
+          <Post
+            key={data.seePost.id}
+            id={data.seePost.id}
+            user={data.seePost.user}
+            pets={data.seePost.pets}
+            files={data.seePost.files}
+            caption={data.seePost.caption}
+            likeCount={data.seePost.likeCount}
+            isLiked={data.seePost.isLiked}
+            permission={data.seePost.permission}
+            commentCount={data.seePost.commentCount}
+            comments={data.seePost.comments}
+            createdAt={data.seePost.createdAt}
+            me={meData.me}
+          />
+        )}
+      </SectionLeft>
+      <SNB>
+        {!meLoading && meData && meData.me && (
+          <>
+            <SideProfile user={meData.me} />
+            <SidePets title={`${meData.me.username}'s Pets`} pets={meData.me.pets} />
+            <SideUsers title={"Following"} users={meData.me.following} />
+            <SideUsers title={"Followers"} users={meData.me.followers} />
+          </>
+        )}
+      </SNB>
+    </>
+  );
+
+  return (
+    <Wrapper>
+      <Helmet>
+        <title>Feed | Farmpet</title>
+      </Helmet>
+      <Contents>{loading || meLoading ? LoaderContents : RealContents}</Contents>
+    </Wrapper>
+  );
+});
