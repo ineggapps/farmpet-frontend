90a170f6bfe9dbf170e0733064e3ef15739ba1f0
inegg <inegg.apps@gmail.com>
2020-03-10 13:40:59 +0900

    도메인 수정

--- src/GlobalVariables.js
+++ src/GlobalVariables.js
@@ -1,6 +1,6 @@
 export const isDebugging = false;
 export const localhostServer = "http://localhost:5000/";
 export const ineggFrontend = "http://farmpet2.inegg.com/";
-export const ineggBackend = "http://farmpet2.inegg.com:5000/";
+export const ineggBackend = "http://farmpet-backend.inegg.com/";
 
 export const getAddress = (uri = "") => (isDebugging ? localhostServer + uri : ineggBackend + uri);
